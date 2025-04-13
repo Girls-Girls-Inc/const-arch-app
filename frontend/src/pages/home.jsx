9f184262dfbb1f1682250290311436a29472c293
seph-ora <sephorak2004@gmail.com>
2025-04-13 21:13:41 +0200

    Added logo to home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
 import "../index.css";
+import logo from "../assets/logo.png"; 
 
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to GG inc</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen text-center">
+      <div className="flex justify-center mb-4">
+        <img src={logo} alt="App Logo" style={{ width: "50px", height: "auto" }} />
+      </div>
+      <h1 className="text-3xl font-semibold mb-6">Welcome to GG inc</h1>
       <div>
-        <p>Links to current Pages:</p>
-        <br></br>
-        <Link className="btn" to="/signIn">
-          Sign In
-        </Link>
-        <Link className="btn" to="/signUp">
-          Sign Up
-        </Link>
+        <p className="mb-4">Links to current Pages:</p>
+        <div className="flex flex-col gap-4 justify-center items-center">
+          <Link className="btn" to="/signIn">
+            Sign In
+          </Link>
+          <Link className="btn" to="/signUp">
+            Sign Up
+          </Link>
+        </div>
       </div>
     </main>
   );
