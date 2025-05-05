364d0ff033c995e935920aa85e32dc4a4293ca2d
Kayisha naidoo <naidookayisha8@gmail.com>
2025-05-05 09:44:13 +0200

    feat: enhance IconButton component with target prop and update styles; add Manage uploads button to navigation refactor: improve loading and error handling in EditUpload and ManageUploads pages

--- frontend/src/components/IconButton.jsx
+++ frontend/src/components/IconButton.jsx
@@ -1,9 +1,15 @@
-// IconButton.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Styles/button.css";
 
-const IconButton = ({ route, icon, label, onClick, type = "button" }) => {
+const IconButton = ({
+  route,
+  icon,
+  label,
+  onClick,
+  type = "button",
+  target = null, // Changed default to null instead of "_blank"
+}) => {
   const buttonContent = (
     <>
       {icon && <i className="material-symbols-outlined">{icon}</i>}
@@ -21,7 +27,11 @@ const IconButton = ({ route, icon, label, onClick, type = "button" }) => {
   }
 
   return (
-    <Link to={route} className="icon-button">
+    <Link
+      to={route}
+      className="icon-button"
+      target={target} // Only added if target is provided
+    >
       {buttonContent}
     </Link>
   );
