1f0de9bf2dfc5f93232089013778c195ec0aaac9
froggomelo <146952438+froggomelo@users.noreply.github.com>
2025-05-24 21:02:52 +0200

    Removed api keys from master

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
 // backend/db.js
 const admin = require('firebase-admin');
+require('dotenv').config();
 
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccountBase64 = process.env.SERVICE_ACCOUNT_KEY;
+
+// Decode base64 and parse JSON
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf-8'));
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
