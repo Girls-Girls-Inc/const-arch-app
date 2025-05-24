044c67b3c6a5b9f234b1a5e5a148b864646f195f
froggomelo <146952438+froggomelo@users.noreply.github.com>
2025-05-24 06:41:51 +0200

    Removed more API keys

--- backend/config.js
+++ backend/config.js
@@ -23,12 +23,4 @@ module.exports = {
     port: PORT,
     host: HOST,
     url: HOST_URL,
-    firebaseConfig: {
-        apiKey: API_KEY,
-        authDomain: AUTH_DOMAIN,
-        projectId: PROJECT_ID,
-        storageBucket: STORAGE_BUCKET,
-        messagingSenderId: MESSAGING_SENDER_ID,
-        appId: APP_ID
-    }
 }
